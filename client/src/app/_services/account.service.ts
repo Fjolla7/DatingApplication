cce4875ad600cb3437a8c4685e10b4f4ddd648a5
Fjolla7 <fjolla.ramadanii@outlook.com>
2020-12-18 17:21:49 +0100

    End of section 16

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, JsonpInterceptor } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -37,7 +37,10 @@ export class AccountService {
     )
   }
 
-  setCurrentUser(user: User) {
+setCurrentUser(user: User) {
+    user.roles = [];
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
@@ -45,5 +48,9 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+  }
+
+  getDecodedToken(token) {
+    return JSON.parse(atob(token.split('.')[1]));
   }
 }
